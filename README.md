40e9159e6fad302cc8f03be1ee3acd89c070b6bd
Ivanov Sergey <djapy@yandex.ru>
2017-07-29 16:17:20 +0300

    First working version

--- README.md
+++ README.md
@@ -1,6 +1,37 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script checks the "health" 
+
+The script returns:
+1. The status of the website.
+2. How many subscription expires.
+
+The program uses the library [python-whois](https://bitbucket.org/richardpenman/pywhois).
+
+# HowTo
+
+To run the script you need python 3.5(and later), Linux.
+
+Use Venv or virtualenv for insulation project.
+Virtualenv example:
+
+```
+$python virtualevn myenv
+$source myenv/bin/activate
+```
+Install requirements:
+
+```
+pip install -r requirements.txt
+```
+if you have error, you need enter `sudo` before command.
+
+An example of running
+
+```
+
+```
+
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,12 +1,12 @@
 from argparse import ArgumentParser
 import requests
 import whois
-import datetime
+from datetime import datetime, timedelta
 
 def parser_command_line():
     parser = ArgumentParser()
-    parser.add_argument('-f', '--file', nargs='?', required=True,
-                        dest='filepath')
+    parser.add_argument('path', help='The path to the directory'
+                        ' for finding file with the list websites', type=str)
     option = parser.parse_args()
     return option
 
@@ -23,25 +23,42 @@ def get_response_whois(url):
 
 
 def is_server_respond_with_200(url):
+    code_200_ok = 200
     request = requests.get(url)
     status_website = request.status_code
-    if status_website == 200:
+    if status_website == code_200_ok:
         return status_website
 
 
 def get_domain_expiration_date(response_whois):
     expiration_date = response_whois.expiration_date
+    type(expiration_date)
+    if type(expiration_date) is list:
+        expiration_date = expiration_date[0]
     return expiration_date
 
 
-def get_result_of_the_check(url):
-    pass
+def get_subscription_period(expiration_date):
+    of_days_in_the_month = timedelta(days=30)
+    remaining_subscription = expiration_date - datetime.today()
+    full_months = remaining_subscription // of_days_in_the_month
+    return full_months
 
 
 if __name__ == '__main__':
     option = parser_command_line()
-    path = option.filepath
+    path = option.path
+    critical_period = 1
     list_urls = get_list_urls(path)
     for url in list_urls:
         response_whois = get_response_whois(url)
-        print(get_domain_expiration_date(response_whois))
+        expiration_date = get_domain_expiration_date(response_whois)
+        full_months = get_subscription_period(expiration_date)
+        print('Website: ', url)
+        print('Status: ', is_server_respond_with_200(url))
+        if full_months > critical_period:
+            print('Until the date of expiry of: {} months'.format(full_months))
+            print('=' * 50)
+        else:
+            print('Attention! Subscription expires!')
+            print('=' * 50)
